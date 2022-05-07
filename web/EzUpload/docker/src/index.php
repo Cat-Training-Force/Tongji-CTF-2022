3d77f081fb4b13d104b190c8f3faaa3d1ae12f69
miaotony <41962043+miaotony@users.noreply.github.com>
2022-05-08 04:22:47 +0800

    Fix web EzUpload.

--- web/EzUpload/docker/src/index.php
+++ web/EzUpload/docker/src/index.php
@@ -1,71 +1,71 @@
-
 <html>
-    <head>
-        <title>
-            EzUpload
-        </title>
-        <style>
-            *{
-                margin: 0;
-                padding: 0;
-            }
-            .ul {
-                margin: 0 auto;
-            }
-            body ul {
-                list-style: none;
-            }
-            a:visited{
-                color: #0000BB;
-            }
-            ul li a {
-                text-decoration: none;
-                margin-right: 10px;
-                text-align: center;
-                width: 150px;
-                list-style: none;
-                padding: 10px;
-                border: 1px dashed #EEE;
-                float: left;
-            }
-            ul li a:hover{
-                background: #EEE;
-            }
-            #index {
-                text-align: center;
-                margin: 0 auto;
-                width: 500px;
-            }
-        </style>
-    </head>
-    <body>
 
+<head>
+    <title>
+        EzUpload
+    </title>
+    <style>
+        .ul {
+            margin: 0 auto;
+        }
 
-        <div id="index" class="ul">
-             <ul>
-                 <li>
-                     <a href="index.php">首页</a>
-                 </li>
+        body ul {
+            list-style: none;
+        }
 
-                 <li>
-                     <a href="index.php?url=upload">上传文件</a>
-                 </li>
-             </ul>
-         </div>
-        <br/>
-    </body>
-</html>
+        a:visited {
+            color: #0000BB;
+        }
+
+        ul li a {
+            text-decoration: none;
+            margin-right: 10px;
+            text-align: center;
+            width: 150px;
+            list-style: none;
+            padding: 10px;
+            border: 1px dashed #EEE;
+            float: left;
+        }
+
+        ul li a:hover {
+            background: #EEE;
+        }
+
+        #index {
+            text-align: center;
+            margin: 0 auto;
+            width: 500px;
+        }
+    </style>
+</head>
+
+<body>
+    <div id="index" class="ul">
+        <h2>EzUpload</h2>
+        <ul>
+            <li>
+                <a href="index.php">首页</a>
+            </li>
+            <li>
+                <a href="index.php?url=upload">上传文件</a>
+            </li>
+        </ul>
+    </div>
+    <br />
+    <br />
 <?php
-    header( 'Content-Type:text/html;charset=utf-8 ');
-    $url = "";
-    if(empty($_REQUEST["url"])){
-        //require("news.php");
-        exit;
-    }
-    else {
-        $url = preg_replace('/%+/', "", $_REQUEST["url"]);
-        require($url . ".php");
-
-    }
-//url=zip://C:\wamp\www/1.zip#1
+error_reporting(0);
+// header('Content-Type:text/html;charset=utf-8');
+$url = "";
+if (empty($_REQUEST["url"]) || $_REQUEST["url"] === "index") {
+    echo '<h3 style="text-align: center;">快让我看看你藏了什么好东西！</h2>';
+    exit;
+} else {
+    $url = preg_replace('/%+/', "", $_REQUEST["url"]);
+    require($url . ".php");
+}
 ?>
+</body>
+
+</html>
--- web/EzUpload/docker/start.sh
+++ web/EzUpload/docker/start.sh
@@ -6,5 +6,4 @@ export FLAG=not_flag
 FLAG=not_flag
 
 /etc/init.d/apache2 restart
-service mysql restart
 /usr/bin/tail -f /dev/null
